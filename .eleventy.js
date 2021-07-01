23052b060accb34756d32f8c306437610fabddb9
Nico Braun <nico-braun@live.de>
2021-07-01 17:14:42 +0000

    rebuild page to minimalist setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,78 +1,43 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const Prism = require('prismjs');
-const htmlmin = require("html-minifier");
-
-
 
 module.exports = function (eleventyConfig) {
 
-  const isProduction = process.env.NODE_ENV==='production'
+    eleventyConfig.setTemplateFormats([
+        "md",
+        "css",
+        "njk",
+        "svg",
+    ]);
 
-  require('dotenv').config()
+    eleventyConfig.addPlugin(syntaxHighlight)
 
-  // formats
-  eleventyConfig.setTemplateFormats([
-    'html',
-    'md',
-    'njk',
-  ]);
+    // use to slugify anchors
+    const slugify = eleventyConfig.javascriptFunctions.slug;
 
-  // deep merge to create directory based collections
-  eleventyConfig.setDataDeepMerge(true);
+    const pluginTOC = require('eleventy-plugin-nesting-toc');
+    eleventyConfig.addPlugin(pluginTOC);
 
+    const markdownIt = require('markdown-it');
+    const markdownItAnchor = require('markdown-it-anchor');
+    eleventyConfig.setLibrary("md",
+        markdownIt({
+            html: true,
+            linkify: true,
+            typographer: true,
+        }).use(markdownItAnchor, {
+            slugify,
+            permalink: true,
+            permalinkClass: "tdbc-anchor",
+            permalinkSymbol: "#",
+            permalinkSpace: true,
+            permalinkBefore: false,
+        })
+    );
 
-  // this is apparently slow, 
-  //eleventyConfig.addPassthroughCopy("views/**/*.(js|css|png|img|jpg|jpeg|ico)");
-  
-  //its better to use a dedicated folder.
-  eleventyConfig.addPassthroughCopy({"assets": "."});
-
-  // watch asset dir
-  eleventyConfig.addWatchTarget("assets");
-
-  // Layouts
-  eleventyConfig.addLayoutAlias('base', 'base.njk')
-  eleventyConfig.addLayoutAlias('post', 'md-card.njk')
-
-  // plugins 
-  eleventyConfig.addPlugin(syntaxHighlight);
-
-  // shortCodes
-  eleventyConfig.addNunjucksShortcode("highligh", (html, lang = 'javascript') => {
-    return Prism.highlight(html, Prism.languages[lang], lang);
-  })
-
-  eleventyConfig.addNunjucksShortcode("trimmedIso", (unixTime) => {
-    return new Date(unixTime * 1000).toISOString().substring(0,10)
-  })
-
-
-  // transforms
-  eleventyConfig.addTransform("mini", function (content, outputPath) {
-    if (isProduction && outputPath.endsWith(".html")) {
-      return htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-        minifyJS: true,
-        minifyCSS: true,
-      });
+    return {
+        dir: {
+            input: 'src',
+            output: "dist"
+        },
     }
-
-    return content;
-  });
-
-
-  //base
-  return {
-    dir: {
-      input: 'views',
-      output: "dist"
-
-    },
-    passthroughFileCopy: true,
-    markdownTemplateEngine: "md",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-  };
 }
